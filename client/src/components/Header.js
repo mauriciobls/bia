ee7e0c2bf55016cabf039fbdeec116e73b3a59ad
mauriciobls <mauriciolopesbls@gmail.com>
2022-09-10 22:59:14 -0300

    Adicionar NF

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "red" : "green"}
-          text={showAdd ? "Fechar" : "Add 1"}
+          text={showAdd ? "Fechar" : "Adicionar NF"}
           onClick={onAdd}
         />
       )}
