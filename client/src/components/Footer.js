959a1f192e1832431a2f63e7514e42c352865bcd
mauriciobls <mauriciolopesbls@gmail.com>
2022-09-10 22:41:40 -0300

    Alteração realizada no Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright Oregon Formação AWS 3.0 2023</p>
+      <p>Copyright Oregon Formação AWS 3.0 2050</p>
       <Link to="/about">Sobre o BIA</Link>
     </footer>
   );
