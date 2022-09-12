f4e217602b229b701cbb9aa2c09486f0ced74bf0
mauriciobls <mauriciolopesbls@gmail.com>
2022-09-11 23:31:45 -0300

    alteracao para NFs não cadastradas

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ function App() {
                   onToggle={toggleReminder}
                 />
               ) : (
-                "Sem Tarefas Cadastradas"
+                "Sem NFs Cadastradas"
               )}
             </>
           )}
